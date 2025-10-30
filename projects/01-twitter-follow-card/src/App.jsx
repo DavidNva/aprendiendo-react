c900b913511ae8017267009d468f4f54a6f30b85
David Nava <david.nava.garcia4@gmail.com>
2025-10-30 00:34:13 -0600

    Creación de primer componente basico en react

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -2,16 +2,16 @@ import './App.css'
 
 export function App(){
     return (
-        <article style={{display:'flex', alignItems:'center', color:'#fff'}}>
-            <header>
-                <img src="https://davidnavadev.netlify.app/images/perfil_original.webp"  style={{ width: "120px", height: "120px", borderRadius: "8px"}}></img>
-                <div>
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img className='tw-followCard-avatar' alt="El avatar de David" src="https://davidnavadev.netlify.app/images/perfil_original.webp"></img>
+                <div className='tw-followCard-info'>
                     <strong>David Nava Garcia</strong>
-                    <span>@davidng</span>
+                    <span className='tw-followCard-infoUserName'>@davidng</span>
                 </div>
             </header>
             <aside>
-                <button>
+                <button className='tw-followCard-button'>
                     Seguir
                 </button>
             </aside>

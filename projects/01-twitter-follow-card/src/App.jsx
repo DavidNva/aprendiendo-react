b6be55a1721f40e038a1f6f5aa464d8e16aa2203
David Nava <david.nava.garcia4@gmail.com>
2025-10-31 14:36:09 -0600

    Como pasar por parametros los props como un objeto
    Podemos pasar las props como un objeto pero no es muy recomendable o buena practica, a menos que en verdad fuera mas claro, asi, pero en cierto punto renderizaria cosas que no react o cuando no debe, o temas de estructura de codigo

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -3,16 +3,18 @@ import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 export function App(){
+    const david = {isFollowing:true, userName:"DavidNvaG"}
+    const elonmusk = {isFollowing:false, userName:"elonmusk"}
     return (
         //Esta siguiente forma es de hacer React.Fragment
         <section className='App'>
-            <TwitterFollowCard isFollowing userName="DavidNvaG">
+            <TwitterFollowCard {...david}>
                David Nava Garcia
                 {/* <TwitterFollowCard isFollowing={false} userName="elonmusk" name="Elon Musk"></TwitterFollowCard> Por ejemplo, aqui incluimos un componente dentro de otro componente y es OK, es literalmetne como lo hacemos con HTML, un elemento padre (por ejemplo un div) y elementos hijos, h1, etc. por ejemplo un header table, tiene su rows, sus th, sus, rr, th*/}
                {/* Incluso mas cosas es como si fuera un campo libre. Como si dejamos que el componente sea aun mas reutilizable, cuando queramos que el componente sea  muy extensible, entonces usamos esta forma de children */}
                 {/* en children podemos poner texto, otro elemento, ejemplo un <p>David Nava Garcia</p>, otro componente, etc, etc. Esto es justamente la compotenizacion */}
             </TwitterFollowCard>
-            <TwitterFollowCard isFollowing={false} userName="elonmusk">
+            <TwitterFollowCard {...elonmusk}>
                 Elon Musk
             </TwitterFollowCard>
 

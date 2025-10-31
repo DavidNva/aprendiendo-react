2e3acb12693e9a9a5c29cf3325eb898d2740359c
David Nava <david.nava.garcia4@gmail.com>
2025-10-31 14:32:03 -0600

    Uso de children para hacer max flexible un componente o extensible

--- projects/01-twitter-follow-card/src/TwitterFollowCard.jsx
+++ projects/01-twitter-follow-card/src/TwitterFollowCard.jsx
@@ -1,11 +1,12 @@
-export function TwitterFollowCard({formatUserName,userName, name, isFollowing}){
+export function TwitterFollowCard({children,userName = 'unknown', isFollowing}){
     return (
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
                 <img className='tw-followCard-avatar' alt="El avatar de David" src={`https://unavatar.io/x/${userName}`}></img>
                 <div className='tw-followCard-info'>
-                    <strong>{name}</strong>
-                    <span className='tw-followCard-infoUserName'>{formatUserName(userName)}</span>
+                   <strong>{children}</strong>
+                   {/* si queremos usar childrens especificamente como lo conocemos, debemos usar solo esa palabra reservada children */}
+                    <span className='tw-followCard-infoUserName'>@{userName}</span>
                 </div>
             </header>
             <aside>

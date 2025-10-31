cee9ad7536adc0cdb66e3fc5863e7352273ebd3d
David Nava <david.nava.garcia4@gmail.com>
2025-10-31 13:39:31 -0600

    Ejemplo de como Podemos pasar funciones como props

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
-import './App.css'
-import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+import React from "react";
+import "./App.css";
+import { TwitterFollowCard } from "./TwitterFollowCard.jsx";
 
-export function App(){
-    return (
-        //Esta siguiente forma es de hacer React.Fragment
-        <section className='App'>
-            <TwitterFollowCard userName="DavidNvaG" name="David Nava Garcia"></TwitterFollowCard>
-            <TwitterFollowCard userName="elonmusk" name="Elon Musk"></TwitterFollowCard>
-            <TwitterFollowCard userName="midudev" name="Miguel Angel Duran"></TwitterFollowCard>
-        </section>
-        
-    )
+export function App() {
+  //crear props como funciones:
+  const formatUserName = (userName) => `@${userName}`;
+  return (
+    //Esta siguiente forma es de hacer React.Fragment
+    <section className="App">
+      <TwitterFollowCard
+        formatUserName={formatUserName}
+        isFollowing
+        userName="DavidNvaG"
+        name="David Nava Garcia"
+      ></TwitterFollowCard>
+
+      <TwitterFollowCard
+        formatUserName={formatUserName}
+        isFollowing={false}
+        userName="elonmusk"
+        name="Elon Musk"
+      ></TwitterFollowCard>
+      
+      <TwitterFollowCard
+        formatUserName={formatUserName}
+        isFollowing
+        userName="midudev"
+        name="Miguel Angel Duran"
+      ></TwitterFollowCard>
+    </section>
+  );
 }
--- projects/01-twitter-follow-card/src/TwitterFollowCard.jsx
+++ projects/01-twitter-follow-card/src/TwitterFollowCard.jsx
@@ -1,11 +1,11 @@
-export function TwitterFollowCard({userName, name, isFollowing}){
+export function TwitterFollowCard({formatUserName,userName, name, isFollowing}){
     return (
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
                 <img className='tw-followCard-avatar' alt="El avatar de David" src={`https://unavatar.io/x/${userName}`}></img>
                 <div className='tw-followCard-info'>
                     <strong>{name}</strong>
-                    <span className='tw-followCard-infoUserName'>@{userName}</span>
+                    <span className='tw-followCard-infoUserName'>{formatUserName(userName)}</span>
                 </div>
             </header>
             <aside>

6c17815ba8af9b54aecd3c345fcc4f96440f6189
David Nava <david.nava.garcia4@gmail.com>
2025-11-02 22:46:20 -0600

    Ejemplo usando un array de objetos
    Para simular que retorna el backend el objeto con los datos del usuario

--- projects/01-twitter-follow-card/src/AppNotes3.jsx
+++ projects/01-twitter-follow-card/src/AppNotes3.jsx
@@ -26,8 +26,7 @@ export function App(){
                 users.map(({userName, name, isFollowing})=>(
                         <TwitterFollowCard 
                         key={userName}//en este caso, una red social: twitter, no tiene sentido o no es posible repetir un nombreUsuario ya registrado (en una base de datos podemos usar el id de este). 
-                        //Podemos usar username + avatar, etc. el punto es que sea un idUnico.
-                        //Lo mejor es usar el id desde la base de datos. 
+                        //Podemos usar username + avatar, etc. el punto es que sea un idUnico
                         userName={userName}
                         initialIsFollowing={isFollowing}
                         >
@@ -39,3 +38,17 @@ export function App(){
         </section> 
     )
 }
+
+//NOTA: Cuando renderizamos una lista de elementos (un array) en react, tenemos que añadir tambien la key. Esto debido a que es un identificador unico para este elemento. Es importante para que react sepa, nosotros tenemos que indicarle que vas a utilizar el DOM virtual, para que seas capaz de saber exactamente a que elementos me estoy referiendo en un array, te voy a identificar el elemento del array, y eso es con algo que sea unico, es decir una key (como una primary key en BD SQL)
+/*       users.map(user=>{
+//                     const {userName, name, isFollowing} = user
+//                     return(
+//                         <TwitterFollowCard 
+//                         userName={userName}
+//                         initialIsFollowing={isFollowing}
+//                         >
+//                             {name}
+//                         </TwitterFollowCard>
+//                     )
+//                 })
+// }}/*///podemos usar el return directamnet eo no indicar ese return, solo colocarlo como se dejo arribas
